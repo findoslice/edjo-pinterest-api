688abd4ee200802fe1af1af93096646536df109a
Findlay Smith <findlayksmith@gmail.com>
2019-01-17 14:17:41 +0000

    refactored image parsing to use pillow and read images from the web

--- image_parser.py
+++ image_parser.py
@@ -1,19 +1,31 @@
-from skimage.io import imread
+from PIL import Image
+from requests import get
+from io import BytesIO
+from time import time
 
 def read_image(file_name):
-    image_np = imread(file_name)
-    image = [hexify(list(row)) for row in image_np]
+    image = Image.open(file_name)
     return image
 
-def get_colours(image):
-    pixels = read_image(image)
-    pixels = [code for row in pixels for code in row]
-    colours = list(set(pixels))
-    return colours
+def read_web_image(url):
+    response = get(url)
+    img = Image.open(BytesIO(response.content))
+    return img
+
+def get_colours(image, weborfile = read_web_image):
+    # allows function to be used for either remote or local images
+    image = weborfile(image)
+    # returns typle (width, size)
+    size = image.size
+    # returns all present colours and their frequency,if there are more colours than maxcolors (default 256) this returns None
+    # this is avoided by setting maxcolors to the number of pixels
+    pixels = image.getcolors(maxcolors=(size[0]*size[1]))
+    pixels = hexify(pixels)
+    return pixels
 
 def hexify(row):
     for i in range(len(row)):
-        row[i] = rgbtohex(row[i])
+        row[i] = (row[i][0], rgbtohex(row[i][1]))
     return row
 
 def rgbtohex(pixel):
@@ -21,3 +33,7 @@ def rgbtohex(pixel):
     for num in pixel:
         hexcode += str(hex(num))[2:].upper()
     return hexcode
+
+time1 = time()
+get_colours("https://tardis.ed.ac.uk/~findoslice/profile.jpg")
+print(time() - time1)
--- log.md
+++ log.md
@@ -8,3 +8,18 @@
 - found basic pinterest api and a python client library
 - As this may be slow for processing images I decided to use falcon to try and shave off latency
 - after some research I decided to use scikit-image for image parsing
+
+### 17/01/19
+
+- Decided to rewrite colour processing to read the image from directly from the web, to save time and resources as saving/deleting files is no longer necessary, nor is reading from a file
+- Discovered pillow is a better choice as it is possible with this, as well as having an inbuilt getcolors method
+
+## Work
+
+### 16/01/19
+
+- implemented colour detection for locally stored files using scikit-image
+
+### 17/01/19
+
+- Rewrote image_parser.py to be able to read from the web or locally and to use pillow
