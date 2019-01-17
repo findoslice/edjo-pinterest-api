d0145333ca40f1ca438e8e7ea87e3b77d66ed514
Findlay Smith <findlayksmith@gmail.com>
2019-01-17 00:18:28 +0000

    added function stubs for pinterest queries

---
+++ api.py
@@ -0,0 +1,14 @@
+import falcon
+
+def search_board(board, colours):
+    return None
+
+def search_user(user, colours):
+    return None
+
+def search_term(term, colours):
+    return None
+
+#not sure if this is possible in practice without indexing all of pinterest
+def search_all(colours):
+    return None
