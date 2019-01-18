fb2ae1958628ed9a7009d62ae49f39f6cc7d39bc
Findlay Smith <findlayksmith@gmail.com>
2019-01-18 00:16:39 +0000

    it's all pretty buggy but I fixed my selenium issues and have a plan for how to make a crawler

--- scraper.py
+++ scraper.py
@@ -1,14 +1,25 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from requests import get
+import os
 
 def get_pinterest_image(url):
-    browser = webdriver.Firefox()
+    options = webdriver.chrome.options.Options()
+    options.add_argument("--headless")
+    options.add_argument("--test-type")
+    options.add_argument('--ignore-certificate-errors')
+    options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+    browser = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=options)
+    wait = WebDriverWait(browser, 10)
     browser.get(url)
-    print(browser.title)
-    images = browser.find_element_by_css_selector("_u3 _45 _y8 _4h")
-    print(images)
+    #wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "_4f _h _xu _4q")))
+    print(browser.page_source)
+    #images = browser.find_element_by_css_selector("_u3 _45 _y8 _4h")
+    #print(images)
     browser.close()
 
-#get_pinterest_image("https://www.pinterest.co.uk/pin/353321533260110279/")
-get_pinterest_image("https://findoslice.com")
+get_pinterest_image("https://www.pinterest.co.uk/search/pins/?q=yeet")
+#get_pinterest_image("https://findoslice.com")
